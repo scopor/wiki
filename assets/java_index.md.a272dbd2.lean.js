d1dc87c78eebb62f0b9e702e9edec96fffcad437
scopor <scopor@users.noreply.github.com>
2022-10-14 14:25:52 +0000

    deploy: 32a1fe6d335338c25746db883fe915494acde402

---
+++ assets/java_index.md.a272dbd2.lean.js
@@ -0,0 +1,6 @@
+import{o as a,c as s,a as e,b as i}from"./app.e3e9c775.js";const n=e("h2",{id:"init",tabindex:"-1"},[i("init "),e("a",{class:"header-anchor",href:"#init","aria-hidden":"true"},"#")],-1),o=[n],m=JSON.parse('{"title":"init","description":"","frontmatter":{},"headers":[{"level":2,"title":"init","slug":"init"}],"relativePath":"java/index.md","lastUpdated":1665757526000}'),d={name:"java/index.md"},p=Object.assign(d,{setup(r){var t=document.getElementsByClassName("VPDocFooter").get(0);return t.innerHtml=`<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-6765261154701378"
+     data-ad-slot="8034548941"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`,(l,c)=>(a(),s("div",null,o))}});export{m as __pageData,p as default};
