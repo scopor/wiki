87796a79273702f2a24ff23e4df6c785505cbc4f
scopor <39816569+scopor@users.noreply.github.com>
2022-10-28 16:17:57 +0800

    Update sitemap.ts

--- docs/.vitepress/config/sitemap.ts
+++ docs/.vitepress/config/sitemap.ts
@@ -8,7 +8,6 @@ const hostname = 'https://wiki.sanoon.me/';
 export const transformHtml = (_, id, { pageData }) => {
     if (!/[\\/]404\.html$/.test(id)) {
         links.push({
-            // you might need to change this if not using clean urls mode
             url: pageData.relativePath.replace(/((^|\/)index)?\.md$/, '$2'),
             lastmod: pageData.lastUpdated
         })
@@ -16,12 +15,9 @@ export const transformHtml = (_, id, { pageData }) => {
 }
 
 export const buildEnd = ({ outDir }) => {
-    console.log(outDir)
     const sitemap = new SitemapStream({ hostname })
     const writeStream = createWriteStream(resolve(outDir, 'sitemap.xml'))
     sitemap.pipe(writeStream)
-    console.log(links)
     links.forEach((link) => sitemap.write(link))
     sitemap.end()
-    console.log("finish")
 }
