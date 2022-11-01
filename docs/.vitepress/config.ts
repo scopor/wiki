fa4f6a2593eb2de711f4e9b79083b6cd8e1358bc
scopor <39816569+scopor@users.noreply.github.com>
2022-11-01 17:43:06 +0800

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -87,6 +87,7 @@ export default defineConfig({
             '/whisper/': sidebarWhisper(),
             '/photo/': sidebarPhoto(),
             '/java': sidebarJava(),
+            '/reading': sidebarReading(),
         },
 
         socialLinks: [
