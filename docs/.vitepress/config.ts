ade64050dea92d99449e73fee3001795cea8d392
scopor <39816569+scopor@users.noreply.github.com>
2023-07-01 09:14:03 +0800

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -165,7 +165,8 @@ function sidebarVientiane() {
                 { text: 'Mac sips 命令', link: '/vientiane/sips' },
                 { text: '查询进程启动时间', link: '/vientiane/查询进程启动时间' },
                 { text: '查找最新修改的文件', link: '/vientiane/查找最新修改的文件' },
-                { text: '删除 MacOS 残留的图标', link: '/vientiane/删除MacOS残留的图标' }
+                { text: '删除 MacOS 残留的图标', link: '/vientiane/删除MacOS残留的图标' },
+                { text: '信号', link: '/vientiane/信号量' }
             ]
         },
         {
