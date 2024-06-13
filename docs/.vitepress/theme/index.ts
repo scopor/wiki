a2e857369c8f38eee625c0356dd8897d412baa0b
scopor <39816569+scopor@users.noreply.github.com>
2024-06-13 16:17:16 +0800

    Update index.ts

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,10 +1,30 @@
-import DefaultTheme from 'vitepress/theme';
+import {h} from 'vue'
+import {useRoute} from 'vitepress'
+import DefaultTheme from 'vitepress/theme'
 import GoogleAdsense from './components/GoogleAdsense.vue';
 import './style/custom.css';
+import './style.css'
+import 'viewerjs/dist/viewer.min.css';
+import imageViewer from 'vitepress-plugin-image-viewer';
+import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer.vue';
 
 export default {
-    ...DefaultTheme,
-    enhanceApp({ app }) {
-        // app.component('GoogleAdsense', GoogleAdsense);
+    extends: DefaultTheme,
+    Layout: () => {
+        return h(DefaultTheme.Layout, null, {
+            // https://vitepress.dev/guide/extending-default-theme#layout-slots
+        })
+    },
+    enhanceApp(ctx) {
+        DefaultTheme.enhanceApp(ctx);
+        // Register global components, if you don't want to use it, you don't need to add it
+        ctx.app.component('vImageViewer', vImageViewer);
+        // ...
+    },
+    setup() {
+        // Get route
+        const route = useRoute();
+        // Using
+        imageViewer(route);
     }
-}
+} satisfies Theme;
