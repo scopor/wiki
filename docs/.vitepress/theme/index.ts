62976081754f3e599fc96cecfd32c0788e918ed6
scopor <39816569+scopor@users.noreply.github.com>
2024-06-18 11:24:13 +0800

    Update index.ts

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -5,6 +5,7 @@ import './style/custom.css';
 import 'viewerjs/dist/viewer.min.css';
 import imageViewer from 'vitepress-plugin-image-viewer';
 import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer.vue';
+import GoogleAdsense from './components/GoogleAdsense.vue';
 
 export default {
     extends: DefaultTheme,
@@ -17,6 +18,7 @@ export default {
         DefaultTheme.enhanceApp(ctx);
         // Register global components, if you don't want to use it, you don't need to add it
         ctx.app.component('vImageViewer', vImageViewer);
+        ctx.app.component('GoogleAdsense', GoogleAdsense);
         // ...
     },
     setup() {
