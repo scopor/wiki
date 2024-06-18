80e00e31b9d14d47407c138441f64708914f09cc
scopor <39816569+scopor@users.noreply.github.com>
2024-06-18 11:23:40 +0800

    Create GoogleAdsense.vue

---
+++ docs/.vitepress/theme/components/GoogleAdsense.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+
+onMounted(() => {
+    const adsbygoogle = (window as any).adsbygoogle as any;
+    adsbygoogle?.push({})
+})
+</script>
+
+<template>
+  <div class="google-adsense">
+     <ins class="adsbygoogle"
+         title="Google Ads" 
+         style="display:block; text-align:center;"
+         data-ad-layout="fluid"
+         data-ad-format="auto"
+         data-ad-client="ca-pub-6765261154701378"
+         data-ad-slot="5012951352" data-full-width-responsive="true">
+     </ins>
+  </div>
+</template>
