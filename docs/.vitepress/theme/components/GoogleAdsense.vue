952075cc8f4f716d514c0a85091ffc83112b66bb
scopor <39816569+scopor@users.noreply.github.com>
2024-06-19 10:37:05 +0800

    Update GoogleAdsense.vue

--- docs/.vitepress/theme/components/GoogleAdsense.vue
+++ docs/.vitepress/theme/components/GoogleAdsense.vue
@@ -11,11 +11,11 @@ onMounted(() => {
   <div class="google-adsense">
      <ins class="adsbygoogle"
          title="Google Ads" 
-         style="display:block; text-align:center;"
+         style="display: block; text-align: center; width: 90%; margin 0 auto; ipcaity: 90%;"
          data-ad-layout="fluid"
          data-ad-format="auto"
          data-ad-client="ca-pub-6765261154701378"
-         data-ad-slot="5012951352" data-full-width-responsive="true">
+         data-ad-slot="5012951352" data-full-width-responsive="false">
      </ins>
   </div>
 </template>
