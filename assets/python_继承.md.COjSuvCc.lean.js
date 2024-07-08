da0f6c009c9db1c0b4254c3b4630e1486c0619d9
scopor <scopor@users.noreply.github.com>
2024-07-08 12:45:34 +0000

    deploy: a95d6f13604c7ee0aeabf2b47ff0530a94374a1e

---
+++ assets/python_继承.md.COjSuvCc.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as i,o as a,a5 as n}from"./chunks/framework.DhMiq51s.js";const o=JSON.parse('{"title":"继承","description":"","frontmatter":{},"headers":[],"relativePath":"python/继承.md","filePath":"python/继承.md"}'),h={name:"python/继承.md"},l=n("",22),k=[l];function p(t,e,d,E,r,g){return a(),i("div",null,k)}const c=s(h,[["render",p]]);export{o as __pageData,c as default};
