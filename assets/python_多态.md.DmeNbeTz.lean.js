3a13964a645463929ba3b0e7c6bd13ad1ab300ce
scopor <scopor@users.noreply.github.com>
2024-07-09 01:15:04 +0000

    deploy: 33dbd85e5325d9f35b2cbca4897231cb81c30068

---
+++ assets/python_多态.md.DmeNbeTz.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as i,o as a,a5 as n}from"./chunks/framework.DhMiq51s.js";const F=JSON.parse('{"title":"Python: 多态","description":"","frontmatter":{},"headers":[],"relativePath":"python/多态.md","filePath":"python/多态.md"}'),h={name:"python/多态.md"},l=n("",19),k=[l];function p(t,e,r,E,d,g){return a(),i("div",null,k)}const c=s(h,[["render",p]]);export{F as __pageData,c as default};
